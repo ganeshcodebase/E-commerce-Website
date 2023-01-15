b2b544e3083b7e8fb6f7a89d7cf0bd0d01947289
Ganesh Guntuku <s170220@rguktsklm.ac.in>
2023-01-16 00:59:47 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var navmenu = document.getElementById("navmenu");
+        function showmenu(){
+            navmenu.style.right = "0";
+        }
+        function hidemenu(){
+            navmenu.style.right = "-300px";
+        }
